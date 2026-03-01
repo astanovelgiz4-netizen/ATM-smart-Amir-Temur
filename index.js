cbc719ab93191e6e8e83a111105aa33c7baca002
astanovelgiz4-netizen <astanovelgiz4@gmail.com>
2026-03-01 07:35:08 +0500

    Add React setup and render App component

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ exports.sendPush = functions.https.onCall(async (data, context) => {
     await admin.messaging().sendToDevice(token, payload);
     return { success: true };
 });
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
